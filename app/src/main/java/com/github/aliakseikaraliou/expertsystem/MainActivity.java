93e48c6b5296841a33e8080a34fa488ed68cdbd1
Aliaksei Karaliou <alexeykoroliov@gmail.com>
2018-02-19 20:47:51 +0300

    Initial commit

---
+++ app/src/main/java/com/github/aliakseikaraliou/expertsystem/MainActivity.java
@@ -0,0 +1,13 @@
+package com.github.aliakseikaraliou.expertsystem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(final Bundle pSavedInstanceState) {
+        super.onCreate(pSavedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
